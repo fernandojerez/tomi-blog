0e2aac986c83180f70cb424ea0865e94d96654a2
Fernando Jerez <fernandojerez@gmail.com>
2020-10-26 08:09:25 -0500

    add global.css file to reset browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,5 +6,6 @@
 
 // You can delete this file if you're not using it
 import { RootElement } from './src/utils/root';
+import './src/styles/global.css';
 
 export const wrapRootElement = RootElement;
--- src/utils/root.js
+++ src/utils/root.js
@@ -4,14 +4,12 @@ import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import Layout from '../components/layout';
 import Store from '../store';
-import { GlobalStyles } from '../styles/global';
 import { Theme } from '../styles/theme';
 
 export const RootElement = ({ element }) => {
 	const persistor = persistStore(Store);
 	return (
 		<>
-			<GlobalStyles />
 			<Provider store={Store}>
 				<PersistGate loading={null} persistor={persistor}></PersistGate>
 				<Theme>
